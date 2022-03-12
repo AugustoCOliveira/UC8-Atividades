15fb2bac62c02d684b174783556a85c908a2ff6d
AugustoCOliveira <90281803+AugustoCOliveira@users.noreply.github.com>
2022-03-12 19:06:11 -0300

    commit

---
+++ peças.js
@@ -0,0 +1,14 @@
+let ListaDePecas = ["Amortecedor", "motor", "filtro de ar"];
+let peso = 50;
+
+if (peso < 100) {
+  console.log("A peca deve pesar no minimo 100g");
+} else {
+  console.log("Cadastro efetuado com sucesso!");
+}
+
+if (ListaDePecas.lenght > 10) {
+  console.log("Nao ha espaço suficiente");
+} else {
+  console.log("Cadastro efetuado com sucesso!");
+}
